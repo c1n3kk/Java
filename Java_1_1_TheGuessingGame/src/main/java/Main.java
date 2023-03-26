8bc6710f20261735f1bf00fa8368ebed262cc7d0
c1n3kk <c1n3kk@gmail.com>
2023-03-26 12:52:15 +0200

    Commit #1: updated project to Java17, minor updates in code

--- Java_1_1_TheGuessingGame/src/main/java/Main.java
+++ Java_1_1_TheGuessingGame/src/main/java/Main.java
@@ -4,20 +4,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int maxNumber = 100;
-        int numberToGuess;
         int tries;
-        int usrAction;
-        int usrNumber;
 
         System.out.println("\nWelcome to 'GUESS THE NUMBER' game!");
         System.out.println("I will think of a number between 0 and 99 and you have to guess it.");
         System.out.println("You have 5 tries. Good luck and have fun!");
         while (true) {
-            numberToGuess = new Random().nextInt(maxNumber);
+            int numberToGuess = new Random().nextInt(maxNumber);
             tries = 5;
             while (tries > 0) {
                 System.out.println("\nPlease enter a number:");
-                usrNumber = new Scanner(System.in).nextInt();
+                int usrNumber = new Scanner(System.in).nextInt();
                 if (usrNumber < 0 || usrNumber > 99) {
                     System.out.println("The number must be between 0 and 99!");
                 } else {
@@ -32,26 +29,26 @@ public class Main {
                 }
             }
             System.out.println("\nDo you want to play again (1 - yes / 0 - no)?");
-            usrAction = new Scanner(System.in).nextInt();
+            int usrAction = new Scanner(System.in).nextInt();
             if (usrAction == 0) {
                 break;
             }
         }
-        System.out.println("Thank you for playing, I hope you had fun. See you next time!");
+        System.out.println("\nThank you for playing, I hope you had fun. See you next time!");
     }
 
     private static int decrementTries(int tries, int number, String hiLow) {
-        String tryOpt;
+        String tryString;
         tries--;
         if (tries == 0) {
             System.out.println("Unfortunately, you have run out of tries. My number was " + number + ".");
         } else {
             if (tries == 1) {
-                tryOpt = "try";
+                tryString = "try";
             } else {
-                tryOpt = "tries";
+                tryString = "tries";
             }
-            System.out.println("Your number is too " + hiLow + "! Try again, you still have " + tries + " " + tryOpt + ".");
+            System.out.println("Your number is too " + hiLow + "! Try again, you still have " + tries + " " + tryString + " left.");
         }
         return tries;
     }
