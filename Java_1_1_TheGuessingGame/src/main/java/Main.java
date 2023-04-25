0de7d446fc1980ef06198f58849c41ef6a78f8af
c1n3kk <c1n3kk@gmail.com>
2023-04-25 13:13:53 +0200

    #Commit 4: TheGuessingGame updated (simplier code)

--- Java_1_1_TheGuessingGame/src/main/java/Main.java
+++ Java_1_1_TheGuessingGame/src/main/java/Main.java
@@ -4,52 +4,53 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int maxNumber = 100;
-        int tries;
+        int tries = 5;
+        int numberToGuess = 0;
+        Boolean gameFinished = false;
 
         System.out.println("\nWelcome to 'GUESS THE NUMBER' game!");
         System.out.println("I will think of a number between 0 and 99 and you have to guess it.");
         System.out.println("You have 5 tries. Good luck and have fun!");
-        while (true) {
-            int numberToGuess = new Random().nextInt(maxNumber);
-            tries = 5;
-            while (tries > 0) {
-                System.out.println("\nPlease enter a number:");
-                int usrNumber = new Scanner(System.in).nextInt();
-                if (usrNumber < 0 || usrNumber > 99) {
-                    System.out.println("The number must be between 0 and 99!");
-                } else {
-                    if (usrNumber > numberToGuess) {
-                        tries = decrementTries(tries, numberToGuess, "high");
-                    } else if (usrNumber < numberToGuess) {
-                        tries = decrementTries(tries, numberToGuess, "low");
-                    } else {
-                        System.out.println("BRAVO! You guessed it! My number was indeed " + numberToGuess + ".");
-                        break;
-                    }
-                }
+
+        while (tries > 0)
+        {
+            if (tries == 5) {
+                numberToGuess = new Random().nextInt(maxNumber);
             }
+
+            System.out.println("\nPlease enter a number:");
+            int usrNumber = new Scanner(System.in).nextInt();
+            tries--;
+
+            if (usrNumber == numberToGuess)
+            {
+                System.out.println("BRAVO! You guessed it! My number was indeed " + numberToGuess + ".");
+                gameFinished = true;
+            }
+
+            if (tries == 0)
+            {
+                System.out.println("Unfortunately, you have run out of tries. My number was " + numberToGuess + ".");
+                gameFinished = true;
+            }
+
+            if (!gameFinished) {
+                printHint(tries, usrNumber > numberToGuess ? "high" : "low");
+                continue;
+            }
+
             System.out.println("\nDo you want to play again (1 - yes / 0 - no)?");
             int usrAction = new Scanner(System.in).nextInt();
-            if (usrAction == 0) {
-                break;
+            if (usrAction != 0) {
+                System.out.println("Starting new game...");
+                tries = 5;
             }
         }
         System.out.println("\nThank you for playing, I hope you had fun. See you next time!");
     }
 
-    private static int decrementTries(int tries, int number, String hiLow) {
-        String tryString;
-        tries--;
-        if (tries == 0) {
-            System.out.println("Unfortunately, you have run out of tries. My number was " + number + ".");
-        } else {
-            if (tries == 1) {
-                tryString = "try";
-            } else {
-                tryString = "tries";
-            }
-            System.out.println("Your number is too " + hiLow + "! Try again, you still have " + tries + " " + tryString + " left.");
-        }
-        return tries;
+    public static void printHint(int tries, String hint)
+    {
+        System.out.println("Your number is too " + hint + "! Try again (tries left: " + tries + ").");
     }
 }
